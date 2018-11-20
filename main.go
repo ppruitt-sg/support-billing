53e760a67298777ad03f8ccd981d456daa9807de
Poul Pruitt <poul.pruitt@sendgrid.com>
2018-11-19 22:03:52 -0700

    Added databases for tickets and comments

--- main.go
+++ main.go
@@ -1,20 +1,34 @@
 package main
 
 import (
+	"database/sql"
 	"html/template"
 	"log"
 	"net/http"
+	"os"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/schema"
 )
 
 type Ticket struct {
+	Number    int64  `schema:"-"`
 	ZDNum     int    `schema:"zdnum"`
 	UserID    int    `schema:"userid"`
 	IssueType string `schema:"issuetype"`
 	Initials  string `schema:"initials"`
 	Comments  string `schema:"comments"`
 }
+
+/*type Comment struct {
+	Timestamp int64
+	Text      string `schema:"comments"`
+}
+
+type Form struct {
+	T Ticket
+	C Comment
+}*/
 
 var tpl *template.Template
 
@@ -47,7 +61,21 @@ func createTicket(w http.ResponseWriter, r *http.Request) {
 			log.Fatalln(err)
 		}
 
-		err = tpl.ExecuteTemplate(w, "submitted.gohtml", nil)
+		db, err := sql.Open("mysql", os.Getenv("DB_USERNAME")+":"+os.Getenv("DB_PASSWORD")+"@/supportbilling")
+		defer db.Close()
+
+		query := `INSERT INTO tickets (zdticket, userid, issuetype, initials, solved)
+		VALUES (?, ?, ?, ?, 0);`
+		result, err := db.Exec(query, t.ZDNum, t.UserID, t.IssueType, t.Initials)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		t.Number, err = result.LastInsertId()
+
+		query = `INSERT INTO comments (text, ticket_id)
+		VALUES (?, ?)`
+		result, err = db.Exec(query, t.Comments, t.Number)
+		err = tpl.ExecuteTemplate(w, "submitted.gohtml", t)
 		if err != nil {
 			log.Fatalln(err)
 		}
