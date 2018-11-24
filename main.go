5a5903bdc7d5b4bf2fc3a302504d0ef8a8ad94f1
Poul Pruitt <poul.pruitt@sendgrid.com>
2018-11-23 22:20:07 -0700

    Added function to get comment from DB and changed Comment.Timestamp from int64 to time.Time

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -18,13 +21,14 @@ type Ticket struct {
 	UserID    int     `schema:"userid"`
 	IssueType string  `schema:"issuetype"`
 	Initials  string  `schema:"initials"`
+	Solved    bool    `schema:"-"`
 	Comment   Comment `schema:"comment"`
 }
 
 type Comment struct {
-	TicketNumber int64
-	Timestamp    int64
+	Timestamp    time.Time
 	Text         string `schema:"text"`
+	TicketNumber int64
 }
 
 var tpl *template.Template
@@ -56,7 +60,7 @@ func parseForm(r *http.Request) (Ticket, error) {
 	if err != nil {
 		return Ticket{}, err
 	}
-	t.Comment.Timestamp = time.Now().UTC().Unix()
+	t.Comment.Timestamp = time.Now()
 
 	return t, nil
 }
@@ -66,7 +70,7 @@ func addCommentToDB(c Comment) error {
 	defer db.Close()
 	query := `INSERT INTO comments (timestamp, text, ticket_id)
 		VALUES (?, ?, ?)`
-	_, err = db.Exec(query, c.Timestamp, c.Text, c.TicketNumber)
+	_, err = db.Exec(query, c.Timestamp.Unix(), c.Text, c.TicketNumber)
 	if err != nil {
 		return err
 	}
@@ -98,6 +102,53 @@ func addTicketToDB(t *Ticket) error {
 	return nil
 }
 
+func getTicketFromDB(num int64) (Ticket, error) {
+	db, err := sql.Open("mysql", os.Getenv("DB_USERNAME")+":"+os.Getenv("DB_PASSWORD")+"@/supportbilling")
+	defer db.Close()
+	if err != nil {
+		return Ticket{}, err
+	}
+
+	query := `SELECT ticket_id, zdticket, userid, issuetype, initials, solved FROM tickets
+		WHERE ticket_id=?`
+	r := db.QueryRow(query, num)
+	t := Ticket{}
+	err = r.Scan(&t.Number, &t.ZDNum, &t.UserID, &t.IssueType, &t.Initials, &t.Solved)
+	if err != nil {
+		return Ticket{}, err
+	}
+
+	t.Comment, err = getCommentFromDB(num)
+	if err != nil {
+		return Ticket{}, err
+	}
+
+	return t, nil
+}
+
+func getCommentFromDB(num int64) (Comment, error) {
+	db, err := sql.Open("mysql", os.Getenv("DB_USERNAME")+":"+os.Getenv("DB_PASSWORD")+"@/supportbilling")
+	defer db.Close()
+	if err != nil {
+		return Comment{}, err
+	}
+
+	query := `SELECT timestamp, text, ticket_id FROM comments
+		WHERE ticket_id=?`
+
+	r := db.QueryRow(query, num)
+	c := Comment{}
+	var ts int64
+	err = r.Scan(&ts, &c.Text, &c.TicketNumber)
+	c.Timestamp = time.Unix(ts, 0)
+	if err != nil {
+		return Comment{}, err
+	}
+	fmt.Println(c.Text)
+
+	return c, nil
+}
+
 func createTicket(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "POST" {
 		// Decode form post to Ticket struct
@@ -115,12 +166,36 @@ func createTicket(w http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			log.Fatalln(err)
 		}
+	} else {
+		w.WriteHeader(http.StatusMethodNotAllowed)
 	}
+}
+
+func displayTicket() func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		ticketNumber, err := strconv.ParseInt(strings.Replace(r.URL.Path, "/view/", "", 1), 10, 64)
+		if err != nil {
+			fmt.Println(err.Error())
+			log.Fatalln(err)
+		}
+		t, err := getTicketFromDB(ticketNumber)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		err = tpl.ExecuteTemplate(w, "viewticket.gohtml", t)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		// If not return 404
+	}
+
 }
 
 func main() {
 	http.HandleFunc("/new", templateHandler("new"))
 	http.HandleFunc("/create", createTicket)
+	http.HandleFunc("/view/", displayTicket())
 
 	http.ListenAndServe(":8080", nil)
 }
