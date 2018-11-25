748c816fcc73ed509d0e2d09a3fb0fd99f45876c
Poul Pruitt <poul.pruitt@sendgrid.com>
2018-11-25 14:16:12 -0700

    Moved template handler into view package

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"log"
 	"net/http"
 
 	"./ticket"
@@ -9,18 +8,8 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-func templateHandler(name string) func(http.ResponseWriter, *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		err := view.Render(w, name+".gohtml", nil)
-		if err != nil {
-			log.Fatalln(err)
-		}
-
-	}
-}
-
 func main() {
-	http.HandleFunc("/new/", templateHandler("new"))
+	http.HandleFunc("/new/", view.TemplateHandler("new"))
 	http.HandleFunc("/create", ticket.Create)
 	http.HandleFunc("/view/", ticket.Display())
 
