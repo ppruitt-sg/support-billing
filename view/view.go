1d1f282696760501ef59eab216d396807056e472
Poul Pruitt <poul.pruitt@sendgrid.com>
2018-12-27 13:20:35 -0700

    Removed templateHandler

--- view/view.go
+++ view/view.go
@@ -2,7 +2,6 @@ package view
 
 import (
 	"html/template"
-	"log"
 	"net/http"
 	"os"
 )
@@ -12,16 +11,6 @@ var tpl *template.Template
 func init() {
 	pwd, _ := os.Getwd()
 	tpl = template.Must(template.ParseGlob(pwd + "/templates/*.gohtml"))
-}
-
-func TemplateHandler(name string) func(http.ResponseWriter, *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		err := Render(w, name+".gohtml", nil)
-		if err != nil {
-			log.Fatalln(err)
-		}
-
-	}
 }
 
 func Render(w http.ResponseWriter, name string, data interface{}) error {
